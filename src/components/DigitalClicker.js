e56a75829f0e376d904e86b0b69d82489aaed152
kainan54 <kaindoneson@gmail.com>
2020-10-05 17:47:40 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    
+    inkzMcgee = () => {
+        this.setState(ps => { return {timesClicked: ps.timesClicked += 1 }})
+    }
+
+    render() {
+        return (
+            <button
+                onClick = {this.inkzMcgee}
+            >{this.state.timesClicked}</button>
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,59 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+    
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+
+      bitBoi = e => {
+        this.setState(ps => {
+            return {
+                settings: {
+                    ...ps.settings,
+                    bitrate: 12,
+                }
+            }
+        })
+      }
+
+      rezzy = e => {
+        this.setState(ps => {
+            return {
+                settings: {
+                    ...ps.settings,
+                    video: {
+                        ...ps.settings.video,
+                        resolution: '720p'
+                    }
+
+                }
+            }
+        })
+      }
+
+      render() {
+          return(
+              <div>
+                <button
+                className = 'bitrate'
+                onClick = {this.bitBoi}
+                ></button>
+
+                <button
+                className = 'resolution'
+                onClick = {this.rezzy}
+                ></button>
+              </div>
+          )
+      }
+}
+
+export default YouTubeDebugger
